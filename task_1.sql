4853d123af086004487e4e64e98445141850a560
Vladyslav Mysholovskyi <64650858+Ev-vl@users.noreply.github.com>
2023-09-19 11:51:49 +0300

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -8,5 +8,5 @@ ALTER COLUMN date_added DATE;
 
 SELECT l.first_name, l.last_name, lcf.linkedin
 FROM leads l
-INNER JOIN leads_custom_fileds lcf ON l.id = lcf.lead_id
-WHERE l.date_added >= (CURRENT_DATE - INTERVAL '7 days');
+INNER JOIN leads_custom_fields lcf ON l.id = lcf.lead_id
+WHERE l.date_added BETWEEN date_add(now(), INTERVAL - 10 DAY) AND now();
